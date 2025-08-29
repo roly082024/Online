706aac134a75d9f6b63a75e6e6ab43f34fb9f241
roly082024 <rolyhuamaccto@gmail.com>
2025-08-29 17:58:57 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,32 @@
+import os
+from dotenv import load_dotenv
 
+# Cargar variables de entorno
+load_dotenv()
+
+# Configuración de la aplicación
+class Config:
+    # Google OAuth
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_SECRET_ID = os.getenv("GOOGLE_SECRET_ID")
+    REDIRECT_URI = os.getenv("REDIRECT_URI", "https://fashion-store-app.streamlit.app")
+    
+    # Stripe
+    STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+    STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
+    STRIPE_SUCCESS_URL = os.getenv("STRIPE_SUCCESS_URL")
+    STRIPE_CANCEL_URL = os.getenv("STRIPE_CANCEL_URL")
+    
+    # Firebase
+    FIREBASE_CONFIG = {
+        "type": os.getenv("FIREBASE_TYPE"),
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
+    }
